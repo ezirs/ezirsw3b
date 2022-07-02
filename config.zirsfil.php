4610b3f78dad458ae401d7ef09a6d1571b770a20
Ezirs <64509305+ezirs@users.noreply.github.com>
2022-07-02 12:11:55 +0700

    Update config.zirsfil.php

--- config.zirsfil.php
+++ config.zirsfil.php
@@ -1,8 +1,8 @@
 <?php
 	$servername = "localhost";
-    $username = "id19097773_root";
-    $password = "Yorandayorick2003Webhost#";
-    $dbname = "id19097773_db_ezirsw3b";
+    $username = "root";
+    $password = "root";
+    $dbname = "db_ezirsw3b";
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 ?>
